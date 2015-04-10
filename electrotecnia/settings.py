8321edac25a0c7d67339b6ef0238dbf175a36157
ramonbadillo <robr8@gmail.com>
2015-04-09 20:58:00 -0500

    error arreglado

--- electrotecnia/settings.py
+++ electrotecnia/settings.py
@@ -108,14 +108,16 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
+STATIC_URL = '/static/'
+
+
+
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
-
-
-STATIC_URL = '/static/'
 
 # Static asset configuration
 import os
@@ -126,3 +128,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
