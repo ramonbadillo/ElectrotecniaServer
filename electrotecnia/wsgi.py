fcb349fcdfc1b147e296d3fd29e23582f2765fa8
Ramon Badillo <ramonbadillo@Ramons-MacBook-Pro.local>
2015-05-27 19:59:14 -0500

    primer commit

--- electrotecnia/wsgi.py
+++ electrotecnia/wsgi.py
@@ -6,12 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "electrotecnia.settings")
 
 from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = Cling(get_wsgi_application())
