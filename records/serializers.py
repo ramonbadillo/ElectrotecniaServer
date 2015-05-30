31315f998208d3692c9dc068320827db2ff46ea6
ramonbadillo <robr8@hotmail.com>
2015-05-29 23:35:37 -0500

    user added to serializer

--- records/serializers.py
+++ records/serializers.py
@@ -1,14 +1,19 @@
 from rest_framework import serializers
 from .models import Record
+from django.contrib.auth.models import User
 #from gadgets.serializers import GadgetSerializerReg
 
+
+class UserSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    username = serializers.CharField(max_length=100)
 
 
 
 class RecordSerializer(serializers.HyperlinkedModelSerializer):
 
 #    idGadget = GadgetSerializerReg()
-
+    user = UserSerializer()
     class Meta:
         model = Record
         fields = ('watts','amp','volts','idKill','timeStampClient','timestampServer','idDev','user')
